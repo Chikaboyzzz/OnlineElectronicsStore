e9a139edc3f2983e6765fb89f07a946dbbc39e3a
Chyngyz <ryskeldiev_ch@iuca.kg>
2023-04-22 02:11:52 +0600

    djangorestframework+ViewsAPI+serializer+urls

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product.apps.ProductConfig',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- backend/config/product/views.py
+++ backend/config/product/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Product, Category
+from .serializer import ProductSerializer
+
+from rest_framework import filters
+
+class ProductAPIList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+
+
+class ProductAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAuthenticated,)
+
+class ProductAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
